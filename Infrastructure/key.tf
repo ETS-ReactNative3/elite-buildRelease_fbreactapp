fd731db339b67e773dd2de45f088f9a6b1c3f79f
ArerepadeB <aredelulu@gmail.com>
2022-01-17 10:42:46 -0600

    fix

--- Infrastructure/key.tf
+++ Infrastructure/key.tf
@@ -1,5 +1,5 @@
 /* keyserver */
-resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
+resource "aws_key_pair" "serverkey" {
+  key_name   = "serverkey"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDsSYI8NZixzmCEAebCO+30zO/yyMdNh6V95N5zz05mzNXAulCp6splHhMFtgvnRKVmWCXEUhwEFfZoosy/l4Wq0u56iTgLAtqfb+x2RG3oxy3TMhQiwQHisiOs3+ae3QKIZL/lzis2jDw7L99MF9/TSLdAZ8RbU+0MOAqiiqE1c2pKknJnTglRRYP0Ce97wKv5vWmrp95peZC0XlGMCjK8FdZ+WwuUeIg6oWrs5B2xJAFzEVXict7lZAv6dftka03jA7XXbMc5tEY0rtfyL1FiRweg9vPK4JuG+zOr2CxZFjbHdEM9YPoppkGH7fX4O9yFyXKHk2sMyH+okstHos52Ok9m4zQXDgVsRISisG8wEmViSiwmSLkfYxJbNZUXgg6MqRfh/6A7lhadqSDGqRQL7CVedHpzp1/X84ixqBHUpFKj4gl0ULYuw98Ty86AMIhc/cIkglzzSxoxAJjM+Qk8BAl9BLorP5Hwv6zTfD69sn0sLGoFJgVgUWcPAKMNY40= lbena@LAPTOP-QB0DU4OG"
 }
--- Infrastructure/server.tf
+++ Infrastructure/server.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.main-public-1.id
-  key_name               = aws_key_pair.mykeypair.key_name
+  key_name               = aws_key_pair.serverkey.key_name
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   user_data_base64       = data.cloudinit_config.userdata.rendered
   lifecycle {
